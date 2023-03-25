7abfccedf0cc2f4de686ec3cd5a1f47b767125e4
Arshia Babaie <arshiababaiz@gmail.com>
2023-03-25 15:51:29 +0330

    Electrical-Circuit-Solver

---
+++ main.py
@@ -0,0 +1,8 @@
+from circuit import Circuit
+
+
+if __name__ == '__main__':
+    my_circuit1 = Circuit('input1.txt')
+    my_circuit2 = Circuit('input2.txt')
+    #cir_nodes_labels = Circuit('input1_new_nodes_labels.txt')
+    cir_nodes_labels = Circuit('input2_new_nodes_labels.txt')
